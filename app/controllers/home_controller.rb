9ca2c1fd2a4ef26fe1ea3dcef4993fbf578039bb
Julie Madsen <julie@lightyearsoftware.com>
2021-06-25 10:04:50 -0400

    Deal with team manager files that don’t have heat/lanes individual timer times
    Signed-off-by: Julie Madsen <julie@lightyearsoftware.com>

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,8 @@
+include ActionView::Helpers::NumberHelper
+
 class HomeController < ApplicationController
 
-
+  
 	def index
 	end
 
@@ -23,37 +25,39 @@ class HomeController < ApplicationController
 	   	output += "B2                                                                                          010101Y1  0.00                      61\r\n"
 
 	   	eventResults = Hash.new
-	   	eventResults = append_to_results_hash(file1, eventResults)
+	   	eventHeats = Hash.new
+	    eventLanes = Hash.new
+	   	eventResults = append_to_results_hash(file1, eventResults, eventHeats, eventLanes, meetDate)
 	   	if (file2)
-	   		eventResults = append_to_results_hash(file2, eventResults)
+	   		eventResults = append_to_results_hash(file2, eventResults, eventHeats, eventLanes, meetDate)
 	   	end
 
 	   	if (file3)
-	   		eventResults = append_to_results_hash(file3, eventResults)
+	   		eventResults = append_to_results_hash(file3, eventResults, eventHeats, eventLanes, meetDate)
 	   	end
 
 	   	if (file4)
-	   		eventResults = append_to_results_hash(file4, eventResults)
+	   		eventResults = append_to_results_hash(file4, eventResults, eventHeats, eventLanes, meetDate)
 	   	end
 
 	   	if (file5)
-	   		eventResults = append_to_results_hash(file5, eventResults)
+	   		eventResults = append_to_results_hash(file5, eventResults, eventHeats, eventLanes, meetDate)
 	   	end
 
 	   	if (file6)
-	   		eventResults = append_to_results_hash(file6, eventResults)
+	   		eventResults = append_to_results_hash(file6, eventResults, eventHeats, eventLanes, meetDate)
 	   	end
 
 	   	if (file7)
-	   		eventResults = append_to_results_hash(file7, eventResults)
+	   		eventResults = append_to_results_hash(file7, eventResults, eventHeats, eventLanes, meetDate)
 	   	end
 
 	   	if (file8)
-	   		eventResults = append_to_results_hash(file8, eventResults)
+	   		eventResults = append_to_results_hash(file8, eventResults, eventHeats, eventLanes, meetDate)
 	   	end
 
 	   	if (file9)
-	   		eventResults = append_to_results_hash(file9, eventResults)
+	   		eventResults = append_to_results_hash(file9, eventResults, eventHeats, eventLanes, meetDate)
 	   	end
 	  	
 	   	output += to_hy3(eventResults)
@@ -65,7 +69,8 @@ class HomeController < ApplicationController
 	end
 
 
-	def append_to_results_hash( fileName, eventResults)
+	def append_to_results_hash( fileName, eventResults, eventHeats, eventLanes, meetDate)
+
 
 		eventResultStr = "";
 		eventNum = 0;
@@ -90,11 +95,27 @@ class HomeController < ApplicationController
 		        		end
 		        		afterSwimmer = line[18..]
 			        	columns = afterSwimmer.gsub(/\s+/m, ' ').gsub(/^\s+|\s+$/m, '').split(" ")
-			        	Rails.logger.info("column size: #{columns.size} #{columns}" )
+			        	#Rails.logger.info("column size: #{columns.size} #{columns}" )
 			        	#if (columns.size >  12)
+			        	
+
+			        	if columns.size < 11 
+			        		eventNum = columns[3]
+			        		line[30..37] = "0S  0.00"
+			        		line[46..50] = "0.00S"
+			        		line[55..59] = "0.00S"
+			        		line[64..67] = "0.00"
+			        		line[64..67] = "0.00"
+			        		line[72..75] = "0.00"
+			        		line[79..80] = "NN"
+			        		line[96] = "N"
+			        		checksum = getHY3checksum(line.rstrip)
+			        		line = line.rstrip[0..-3] + checksum + "\r\n"
+			        	else 
 			        		eventNum = columns[5]
-			        	if eventNum.to_s != eventNum.to_i.to_s
-			        		eventNum = columns[4]
+				        	if eventNum.to_s != eventNum.to_i.to_s
+				        		eventNum = columns[4]
+				        	end
 			        	end
 			        	#else  
 			        	#	eventNum = columns[4]
@@ -104,27 +125,109 @@ class HomeController < ApplicationController
 			        end
 
 			        if (line.start_with?("E2") || line.start_with?("F2"))
+			        	prevLine = line.dup
 
 			        	columns = line.gsub(/\s+/m, ' ').gsub(/^\s+|\s+$/m, '').split(" ")
-			        	if (columns.size > 4)
+			        	if (columns.size > 2)
 			        		time =  columns[1]
+			        		newTimeStr = time.to_s
 			        		if (time[time.length-1] == 'Y')
 			        			timeRaw = time.chomp("Y")
 			        			timeFloat = timeRaw.to_f
 			        			newTime = timeFloat * 1.10999215
-			        			newTimeStr = newTime.round(2).to_s
-			        			line.gsub!(time, newTimeStr+"S")
-			        			checksum = getHY3checksum(line.rstrip)
-			        			line = line.rstrip[0..-3] + checksum + "\r\n"
+			        			newTimeStr = number_with_precision(newTime, :precision => 2).to_str + "S"
+								
+								timeLen = newTimeStr.length 
+								timeIndex = timeLen - 1
+								startIndex = (11- timeLen) + 1
+								for index in (11).downto(startIndex)
+									line[index] = newTimeStr[timeIndex]
+									timeIndex = timeIndex - 1
+								end			        			
 			        			  
 			      
 			        		end
+			        		if !heat = eventHeats[eventNum] 
+			        			heat = 1
+			        		end
+			        		if !lane = eventLanes[eventNum] 
+			        			lane = 0
+			        		end
+
+			        		if lane == 8
+			        			heat = heat + 1
+			        			lane = 1
+			        		else 
+			        			lane = lane + 1
+			        		end
+
+			        		eventHeats[eventNum] = heat
+			        		eventLanes[eventNum] = lane
+
+			        		Rails.logger.info "event #{eventNum} was using heat #{line[22]} lane #{line[25]}"
+			        		Rails.logger.info "event #{eventNum} is now using heat #{heat} lane #{lane}"
+							
+
+							
+
+							line[22] = heat.to_s
+							line[25] = lane.to_s
+				
+							if line[19] == " "
+								line[19] = "0"
+							end
+ 
+							if line[28] == " " || line[28] == "0"
+								line[28] = "1"
+							end
+
+							if line[32] == " " || line[32] == "0"
+								line[32] = "1"
+							end
+
+							if line[35] == " "
+								line[35] = "0"
+							end
+
+							if columns.size < 6
+								timeStrWithoutCourse = newTimeStr.tr('^0-9.', '')  
+								timeLen = timeStrWithoutCourse.length 
+								timeIndex = timeLen - 1
+								startIndex = (43 - timeLen) + 1
+								for index in (43).downto(startIndex)
+									line[index] = timeStrWithoutCourse[timeIndex]
+									timeIndex = timeIndex - 1
+								end
+
+								line[48..51] = "0.00"
+								line[56..59] = "0.00"
+								line[62] = ""
+								line[69..72] = "0.00"
+								line[78..81] = "0.00"
+
+								date = DateTime.parse(meetDate)
+								formatted_date = date.strftime('%m%d%Y')
+								line[87..94] = formatted_date
+								line[122] = "0 " 
+								end 
+							checksum = getHY3checksum(line.rstrip)
+			        		line = line.rstrip[0..-3] + checksum + "\r\n"
+			        		Rails.logger.info "event line length #{line.length}"
+			        		if (line.length > 132) 
+			        			Rails.logger.info "event line length #{line.length}"
+			        		end
+
+			
+
+
+
+			        		
 
 			        	end
 			        	
 			        	eventResultStr = eventResultStr + line
 			        	if (line.start_with?("E2"))
-			        		Rails.logger.info "Adding  for event " + eventNum + ": " + eventResultStr
+			        	#	Rails.logger.info "Adding  for event " + eventNum + ": " + eventResultStr
 			        		eventResults[eventNum] = (eventResults[eventNum] == nil ?  "" : eventResults[eventNum] ) + eventResultStr
 			        		eventResultStr = ""
 
@@ -133,7 +236,7 @@ class HomeController < ApplicationController
 
 			        if (line.start_with?("F3"))
 			        	eventResultStr = eventResultStr + line
-			        		Rails.logger.info "Adding  for event " + eventNum + ": " + eventResultStr
+			        #		Rails.logger.info "Adding  for event " + eventNum + ": " + eventResultStr
 			         		eventResults[eventNum] = (eventResults[eventNum] == nil ?  "" : eventResults[eventNum] ) + eventResultStr
 			        		eventResultStr = ""
 			         end
